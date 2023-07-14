d5c350a3a36b08f0b21722f7f0f063e640078ed9
diegomacias47 <diegomaciaslmk@hotmail.com>
2023-07-14 00:03:03 -0600

    1407 Changes

--- src/pages/payment/Payment.js
+++ src/pages/payment/Payment.js
@@ -1,6 +1,36 @@
 import { Button, FormControl, TextField, FormHelperText, Select, MenuItem, InputLabel, Radio, FormLabel, RadioGroup, FormControlLabel } from "@mui/material";
+import { useState } from "react";
+
+const formValues = {
+    'name_student': '',
+    'mat_student': '',
+    'phone_student': '',
+    'mail_student': '',
+    'suburb_student': '',
+    'cp_student': '',
+    'pay_type': '',
+    'shift': '',
+    'type_study': '',
+    'route': '',
+    'school_id': '',
+    'route_id': '',
+    'payment_file': null,
+}
+
 
 export const Payment = () => {
+
+    const [paymentData, setPaymentData] = useState(formValues);
+
+    const formHandlerChange = (event) => {
+        const data = { ...paymentData };
+
+        data[event.target.name] = event.target.value;
+
+        setPaymentData(data);
+        console.log(paymentData);
+    }
+
     
     const PaymentType = () => {
         return (
@@ -26,43 +56,37 @@ export const Payment = () => {
                 <div className="row">
                     <div className="col-12 col-md-6 pt-3">
                         <FormControl error className="w-100">
-                            <TextField className="w-100" name="student_mat" label="Matricula" variant="outlined" size="small"/>
+                            <TextField className="w-100" name="name_student" label="Nombre" variant="outlined" size="small" onChange={formHandlerChange}/>
                             <FormHelperText></FormHelperText>
                         </FormControl>
                     </div>
                     <div className="col-12 col-md-6 pt-3">
                         <FormControl error className="w-100">
-                            <TextField className="w-100" name="student_mat" label="Matricula" variant="outlined" size="small"/>
+                            <TextField className="w-100" name="mat_student" label="Matricula" variant="outlined" size="small" onChange={formHandlerChange}/>
                             <FormHelperText></FormHelperText>
                         </FormControl>
                     </div>
                     <div className="col-12 col-md-6 pt-3">
                         <FormControl error className="w-100">
-                            <TextField className="w-100" name="student_mat" label="Nombre del alumno" variant="outlined" size="small"/>
+                            <TextField className="w-100" name="phone_student" label="Telefono" variant="outlined" size="small" onChange={formHandlerChange}/>
                             <FormHelperText></FormHelperText>
                         </FormControl>
                     </div>
                     <div className="col-12 col-md-6 pt-3">
                         <FormControl error className="w-100">
-                            <TextField className="w-100" name="student_mat" label="Telefono celular" variant="outlined" size="small"/>
+                            <TextField className="w-100" name="mail_student" label="Correo" variant="outlined" size="small"/>
                             <FormHelperText></FormHelperText>
                         </FormControl>
                     </div>
                     <div className="col-12 col-md-6 pt-3">
                         <FormControl error className="w-100">
-                            <TextField className="w-100" name="student_mat" label="Correo electronico" variant="outlined" size="small"/>
+                            <TextField className="w-100" name="suburb_student" label="Colonia" variant="outlined" size="small"/>
                             <FormHelperText></FormHelperText>
                         </FormControl>
                     </div>
                     <div className="col-12 col-md-6 pt-3">
                         <FormControl error className="w-100">
-                            <TextField className="w-100" name="student_mat" label="Colonia" variant="outlined" size="small"/>
-                            <FormHelperText></FormHelperText>
-                        </FormControl>
-                    </div>
-                    <div className="col-12 col-md-6 pt-3">
-                        <FormControl error className="w-100">
-                            <TextField className="w-100" name="student_mat" label="Codigo postal" variant="outlined" size="small"/>
+                            <TextField className="w-100" name="cp_student" label="Código postal" variant="outlined" size="small"/>
                             <FormHelperText></FormHelperText>
                         </FormControl>
                     </div>
@@ -74,22 +98,8 @@ export const Payment = () => {
                                 label="Grado de educación"
                                 name="type_study"
                             >
-                                <MenuItem value={1}>BACHILLERATO</MenuItem>
-                                <MenuItem value={2}>FACULTAD</MenuItem>
-                            </Select>
-                           <FormHelperText></FormHelperText>
-                        </FormControl>
-                    </div>
-                    <div className="col-md-6 pt-3">
-                        <FormControl fullWidth size="small">
-                            <InputLabel id="demo-simple-select-label">Institución</InputLabel>
-                            <Select
-                                id="type_study"
-                                label="Institución"
-                                name="type_study"
-                            >
-                                <MenuItem value={1}>BACHILLERATO</MenuItem>
-                                <MenuItem value={2}>FACULTAD</MenuItem>
+                                <MenuItem value={'1'}>BACHILLERATO</MenuItem>
+                                <MenuItem value={'2'}>FACULTAD</MenuItem>
                             </Select>
                            <FormHelperText></FormHelperText>
                         </FormControl>
@@ -99,6 +109,20 @@ export const Payment = () => {
 
             <div className="pt-5">
                 <div className="row">
+                    <div className="col-md-6">
+                        <FormControl fullWidth size="small">
+                            <InputLabel id="demo-simple-select-label">Institucion</InputLabel>
+                            <Select
+                                id="type_study"
+                                label="Grado de educación"
+                                name="type_study"
+                            >
+                                <MenuItem value={'1'}>BACHILLERATO</MenuItem>
+                                <MenuItem value={'2'}>FACULTAD</MenuItem>
+                            </Select>
+                           <FormHelperText></FormHelperText>
+                        </FormControl>
+                    </div>
                     <div className="col-md-6 pb-2">
                         <FormControl error className="w-100">
                             <TextField className="w-100" name="student_mat" label="No. referencia" variant="outlined" size="small"/>
